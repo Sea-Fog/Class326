583ed1541ac7f7c2c2d93aed555467cdde7caa46
Sea Fog <larry.greunke@nps.edu>
2020-02-25 09:32:40 -0800

    Added Enemy Manager example and particle effects

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+  public List<Enemy> enemyList;
+
+  void Update()
+  {
+    if (Input.GetAxis("Horizontal") > 0)
+    {
+      foreach (var enemy in enemyList)
+      {
+        enemy.transform.Translate(Vector3.right * 5);
+      }
+    }
+  }
+}
